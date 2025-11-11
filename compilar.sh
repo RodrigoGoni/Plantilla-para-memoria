ae040fb4e190ed3c473f8f74f466ac7d82d96a9a
RodrigoGoni <safoce96@gmail.com>
2025-11-11 13:49:23 -0300

    capitulo dos rev 1

--- compilar.sh
+++ compilar.sh
@@ -1,4 +1,12 @@
-rm -f *.aux *.out *.bbl *.blg *.log *.toc *.lof *.lot *.run.xml
+#!/bin/bash
+
+# Eliminar TODOS los archivos auxiliares
+rm -f *.aux *.bbl *.blg *.out *.toc *.lof *.lot *.bcf *.run.xml *.log *.synctex.gz
+rm -f Chapters/*.aux
+rm -f portada.aux
+rm -f *.aux *.bbl *.blg *.log *.out *.toc *.lof *.lot *.run.xml Chapters/*.aux
+# Compilar
 pdflatex -interaction=nonstopmode memorianueva
 bibtex memorianueva
-pdflatex -interaction=nonstopmode memorianueva && pdflatex -interaction=nonstopmode memorianueva
+pdflatex -interaction=nonstopmode memorianueva
+pdflatex -interaction=nonstopmode memorianueva
